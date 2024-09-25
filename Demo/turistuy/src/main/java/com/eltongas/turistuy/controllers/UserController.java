9bea64ea40fd49cde5681eae50385fa08f0ff418
bruceferr <bruceferr@gmail.com>
2024-09-25 20:50:20 -0300

    Finally

--- Demo/turistuy/src/main/java/com/eltongas/turistuy/controllers/UserController.java
+++ Demo/turistuy/src/main/java/com/eltongas/turistuy/controllers/UserController.java
@@ -2,26 +2,32 @@ package com.eltongas.turistuy.controllers;
 
 import com.eltongas.turistuy.entities.Users;
 import com.eltongas.turistuy.services.IUserServices;
+import com.eltongas.turistuy.services.implement.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
     @Autowired
-    private IUserServices userServices;
+    private UserService userServices;
 
     @PostMapping
     public ResponseEntity<Users> createUser(@RequestBody Users users) {
         Users newUsers = userServices.saveUser(users);
-        System.out.println("Datos " + users);
+        System.out.println("Datos " + users.toString());
+
         return new ResponseEntity<>(newUsers, HttpStatus.OK);
 
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testendpoint() {
+        return new ResponseEntity<>("El endpoint funca!", HttpStatus.OK);
     }
 
 
