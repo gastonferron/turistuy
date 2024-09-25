ae673b7d445bdc7f4fa5c9cbc18cbb453f183d04
bruceferr <bruceferr@gmail.com>
2024-09-24 21:14:47 -0300

    first commit

---
+++ Demo/turistuy/src/main/java/com/eltongas/turistuy/TuristuyApplication.java
@@ -0,0 +1,13 @@
+package com.eltongas.turistuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuristuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuristuyApplication.class, args);
+	}
+
+}
---
+++ Demo/turistuy/src/test/java/com/eltongas/turistuy/TuristuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eltongas.turistuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
